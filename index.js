d900328d1c91aa0db1d9dd0e1ad14353110c32e6
marfpss <69798927+mbbrpiuoficial@users.noreply.github.com>
2022-11-14 01:36:46 -0300

    teste 2

--- index.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-const app = express()
-
-app.disable("x-powered-by")
-app.get("", (_, res) => res.send({ status: "Online" }))
-
-app.use((_, res) => res.send({ status: "Caminho não encontrado." }))
-
-app.listen(8080, "0.0.0.0", () => console.log("Saphire-Site Online"))
