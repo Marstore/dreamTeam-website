4562128fb4b0ae676dff7d9ff4b6a06d2580b98a
marfpss <69798927+mbbrpiuoficial@users.noreply.github.com>
2022-12-03 16:40:37 -0300

    Page Team

---
+++ team/assets/js/bs-init.js
@@ -0,0 +1,19 @@
+
+if (window.innerWidth < 768) {
+	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
+		elem.classList.remove('animated');
+		elem.removeAttribute('data-bss-hover-animate');
+		elem.removeAttribute('data-aos');
+		elem.removeAttribute('data-bss-parallax-bg');
+		elem.removeAttribute('data-bss-scroll-zoom');
+	});
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	AOS && AOS.init();
+
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	  return new bootstrap.Tooltip(tooltipTriggerEl);
+	})
+}, false);
